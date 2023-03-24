4e224887c48b08446981522913b217247dcaa4ae
Shiby-Mathew <shiby.ajay@gmail.com>
2023-03-24 20:16:06 +1100

    added if condition and catch function

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,11 @@ var nasaKey = "Vs5yP6tMYxLhzzevSdf3FfQgHLaYKExehOojOtFT";
 var nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=" + nasaKey;
 var dailyImg = document.getElementById("nasa-img");
 
-
 var rocketName = $("#rocket-name");
 var locationName = $("#location");
 var launchDescription = $("#launch-description");
 
 var dataUrl = "https://fdo.rocketlaunch.live/json/launches/next/5";
-
 
 // Fetch daily NASA image
 function getNasaImg() {
@@ -35,25 +33,38 @@ $.get("https://api.spacexdata.com/v5/launches/latest", function (data) {
   $("#time-to-launch").html(timeUdc);
 });
 
+//Fetch data from API
+
 fetch(dataUrl)
   .then(function (response) {
-    return response.json();
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayData(data);
+      });
+    } else {
+      alert("Error." + response.statusText);
+    }
   })
-  .then(function (data) {
-    console.log(data);
-    displayData(data);
+  .catch(function (error) {
+    alert("Unable to connect to API");
   });
 
-//Display Data in the Dashboard
+//Display Data on the Dashboard
 
 function displayData(data) {
-  // if (data.result[0].t0 === null) {
-  //timeToLaunch.textContent = `Time : ${data.result[0].date_str}`;
-
-  rocketName.text(`Name of the Rocket : ${data.result[0].vehicle.name}`);
-  locationName.text(`Country : ${data.result[0].pad.location.country}`);
+  var dailyResult = data.result[0];
+  //checking if value is null
+  if (dailyResult.vehicle.name === null) {
+    rocketName.text("Data not returned");
+  }
+  if (dailyResult.pad.location.country === null) {
+    locationName.text("Data not returned");
+  }
+  rocketName.text(`Name of the Rocket : ${dailyResult.vehicle.name}`);
+  locationName.text(`Country : ${dailyResult.pad.location.country}`);
   launchDescription.text(
-    `Launch Description : ${data.result[0].launch_description}`
+    `Launch Description : ${dailyResult.launch_description}`
   );
 }
 
